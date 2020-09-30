25643020d9b135266d08f4e4e67b8768a5c80e7a
Augyu <yuhsuan@vt.edu>
2020-09-30 17:05:09 -0400

    add search function/ display no search result

---
+++ js/list.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  handleSearchResult()
+})
+
+// if users search electronics then the list will show up
+// otherwise,
+const handleSearchResult = () => {
+  const queryString = new URLSearchParams(window.location.search)
+  const search = queryString.get('search')
+  changeSearchValue(search)
+  if (search !== 'electronics') {
+    $('.left-container').hide()
+    $('.middle-container').hide()
+    $('.right-container').hide()
+  } else {
+    $('.no-result-container').hide()
+  }
+}
+
+// change the value of the search bar with the search value
+const changeSearchValue = (searchVal) => {
+  $('#search-middle').attr('value', searchVal)
+}
