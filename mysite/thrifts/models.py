979019f0d8716620c7738152586e2fb21d66be28
Augyu <yuhsuan@vt.edu>
2020-10-16 13:55:27 -0400

    add fake comment in model

--- mysite/thrifts/models.py
+++ mysite/thrifts/models.py
@@ -1,42 +1,78 @@
 from django.db import models
 from datetime import datetime
-import uuid
-# Create your models here.
 
 class Product():
     
-    def __init__(self, name, price, img, description, seller, date_posted):
-        self.id = uuid.uuid4
+    def __init__(self, id, name, price, img, description, seller, date_posted, comments):
+        self.id = id
         self.name = name
         self.price = price
         self.img = img
         self.description = description
         self.seller = seller
-        self.date_posted = date_posted  
+        self.date_posted = date_posted
+        self.comments = comments
+    
+    def getProductById(self, id):
+        if self.id == id:
+            return True
+
+# buyers leave comments to sellers base on their products
+class Comment():
+
+    def __init__(self, comment, buyer, seller, date_posted):
+        self.comment = comment
+        self.buyer = buyer
+        self.seller = seller
+        self.date_posted = date_posted
+
+comment1 = Comment('David cleaned the product before he sold it.',
+    'Jenny',
+    'David',
+    datetime(2020, 9, 10))
+comment2 = Comment('I would loved to buy things from David again.',
+    'Frank',
+    'David',
+    datetime(2020, 4, 5))
+comment3 = Comment('David cleaned the product before he sold it.',
+    'Sharon',
+    'David',
+    datetime(2019, 3, 2))
+comment4 = Comment('Really cheap things!',
+    'Tim',
+    'David',
+    datetime(2016, 12, 24))
+comments = [comment1, comment2, comment3, comment4]
 
 products = []
-
 for i in range(1, 20):
     if i % 3 == 1:
-        product = Product("Airpod pro",
+        product = Product(i,
+            "Airpod pro",
             325,    
-            "image/electronics_three.jpg",
-            "I've only used this Airpod pro twice",
-            "David", "2020/10/12 20:45")
+            ["image/electronics_three.jpg"],
+            "This is a 99% new latest Airpods pro. I've only used two times and they don't fit my ears. Still have 11 months warranty. Available Time: Everyeveninig from Monday to Friday",
+            "David",
+            datetime.now(),
+            comments)
     elif i % 3 == 2:
-        product = Product("Amazon Alexa",
+        product = Product(i,
+            "Amazon Alexa",
             25,
-            "image/electronics_two.jpg",
+            ["image/electronics_two.jpg"],
             "Want to buy Google Home. Don't need this anymore.",
             "David",
-            "2020/10/13 16:40")
+            datetime(2020, 10, 10, 13, 10),
+            comments)
     else:
-        product = Product("Air Fryer",
+        product = Product(i,
+            "Air Fryer",
             325,
-            "image/electronics_one.jpg",
+            ["image/electronics_one.jpg"],
             "Moving out this area...",
             "Jeff",
-            "2020/10/13")
+            datetime(2020, 10, 13),
+            comments)
     
     products.append(product)
 
